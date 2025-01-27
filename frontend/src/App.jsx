8e4b9e351272fbdfe463cb8e715bd39ac9d9f0db
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-01-27 22:55:39 +0545

    Routing with Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Navbar from "./components/public/Navbar"
+import Home from "./components/public/Home";
+import Shop from "./components/public/Shop";
+import About from "./components/public/About";
+import Contact from "./components/public/Contact";
+import Login from "./components/public/Login";
+import Signup from "./components/public/Signup";
 
 function App() {
 
   return (
     <>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/shop" element={<Shop/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- frontend/src/components/public/Navbar.jsx
+++ frontend/src/components/public/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
@@ -7,27 +8,31 @@ function Navbar() {
         <div className="text-2xl sm:text-3xl font-bold">Gadgetverse</div>
         <ul className="hidden md:flex text-lg sm:text-2lg">
           <li className="mx-[10px] sm:mx-[20px] cursor-pointer h-10 w-[80px] sm:w-[120px] flex items-center justify-center">
-            HOME
+          <Link to="/home" className="hover:text-red-400">HOME</Link>
           </li>
           <li className="mx-[10px] sm:mx-[20px] cursor-pointer h-10 w-[80px] sm:w-[120px] flex items-center justify-center">
-            SHOP
+          <Link to="/shop" className="hover:text-red-400">SHOP</Link>
           </li>
           <li className="mx-[10px] sm:mx-[20px] cursor-pointer h-10 w-[80px] sm:w-[120px] flex items-center justify-center">
-            ABOUT US
+          <Link to="/about" className="hover:text-red-400">ABOUT US</Link>
           </li>
           <li className="mx-[10px] sm:mx-[20px] cursor-pointer h-10 w-[80px] sm:w-[120px] flex items-center justify-center">
-            CONTACT US
+          <Link to="/contact" className="hover:text-red-400">CONTACT US</Link>
           </li>
         </ul>
 
         <div className="flex items-center space-x-5">
           <div className="flex items-center">
+          <Link to="/login" className="font-bold mr-2 hover:text-pink-500 hover:cursor-pointer">
             <i className="fa-solid fa-user pr-2"></i>
             <span className="hidden sm:inline">Login</span>
+          </Link>
           </div>
           <div className="flex items-center px-4">
+          <Link to="/signup" className="font-bold mr-2 hover:text-pink-500 hover:cursor-pointer">
             <i className="fa-solid fa-user-plus pr-2"></i>
             <span className="hidden sm:inline">Signup</span>
+          </Link>
           </div>
         </div>
       </nav>
