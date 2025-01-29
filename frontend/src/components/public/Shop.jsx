801b001796013f498be3bd736606e89e99de132d
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-01-29 21:39:53 +0545

    Shop Section with ShopCard

--- frontend/src/components/public/Shop.jsx
+++ frontend/src/components/public/Shop.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import ShopCard from './ShopCard'
+import Footer from './Footer'
 
 function Shop() {
   return (
-    <div>
-      
+    <>
+    <div className= 'bg-gradient-to-r from-blue-400 via-red-300 to-blue-400 min-h-screen'>
+    {/* Search Bar */}
+    <div className="w-full flex flex-col sm:flex-row justify-center px-4 py-5 gap-2">
+      <input
+      type="text"
+      placeholder="Search gadgets here"
+      className="flex-grow px-4 py-2 rounded-lg sm:rounded-l-lg bg-white text-black outline-none w-full sm:w-auto"
+      />
+      <button className="p-2 bg-slate-900 text-white rounded-lg sm:rounded-r-lg hover:bg-gray-800 w-full sm:w-auto hover:cursor-pointer transition-all duration-300 ease-in-out transform hover:scale-105">
+        Search
+      </button>
     </div>
+
+    <div className="flex flex-wrap md:gap-0 md:px-0 px-4 gap-2">
+      <ShopCard/>
+      <ShopCard/>
+      <ShopCard/>
+      <ShopCard/>
+      <ShopCard/>
+    </div>
+    
+    <div className="bg-gradient-to-r from-blue-400 via-red-300 to-blue-400 mt-8">
+    <Footer/>
+    </div>
+    </div>
+    </>
   )
 }
 
---
+++ frontend/src/components/public/ShopCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function ShopCard() {
+  return (
+    <div className="bg-white text-black rounded-lg p-4 w-full sm:w-[300px] h-auto sm:h-[420px] ml-0 sm:ml-3 mb-3 flex flex-col justify-between shadow-lg">
+      <img
+        src="https://via.placeholder.com/300x200"
+        alt="Product"
+        className="w-full h-[200px] object-cover rounded"
+      />
+      <div>
+        <p className="text-lg font-semibold">MacBook</p>
+        <p><strong>Category:</strong> Laptop</p>
+        <p><strong>Price:</strong> RS 180000</p>
+        <p><strong>Description:</strong> Best Selling Laptop</p>
+      </div>
+      <Link to="/placeorder">
+        <button className="bg-pink-500 text-white w-full hover:bg-slate-900 py-2 rounded-lg hover:cursor-pointer transition-all duration-300 ease-in-out transform hover:scale-105">
+          Buy Now
+        </button>
+      </Link>
+    </div>
+  );
+}
+
+export default ShopCard;
