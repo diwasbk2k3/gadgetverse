59d18043d5daa4729971d50622f03a5b285519d4
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-02-02 01:14:52 +0545

    Orders and Users

--- frontend/src/components/private/admin/Orders.jsx
+++ frontend/src/components/private/admin/Orders.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 
 function Orders() {
   return (
-    <div>
-        
+    <div className="p-6">
+      <h2 className="text-2xl font-bold mb-6 text-center">Orders</h2>
+      <div className="overflow-x-auto">
+        <table className="w-full border-collapse border border-gray-300 mt-2">
+          <thead className="bg-gray-200">
+            <tr>
+              <th className="border border-gray-300 px-4 py-2">SN.</th>
+              <th className="border border-gray-300 px-4 py-2">Order ID</th>
+              <th className="border border-gray-300 px-4 py-2">Customer ID</th>
+              <th className="border border-gray-300 px-4 py-2">Product ID</th>
+              <th className="border border-gray-300 px-4 py-2">Product Name</th>
+              <th className="border border-gray-300 px-4 py-2">Quantity</th>
+              <th className="border border-gray-300 px-4 py-2">Price</th>
+              <th className="border border-gray-300 px-4 py-2">Total Price</th>
+              <th className="border border-gray-300 px-4 py-2">Phone</th>
+              <th className="border border-gray-300 px-4 py-2">Email</th>
+              <th className="border border-gray-300 px-4 py-2">Location</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td colSpan="11" className="text-center py-4">
+                No orders available
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
--- frontend/src/components/private/admin/Users.jsx
+++ frontend/src/components/private/admin/Users.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 function Users() {
   return (
-    <div>
-        
+    <div className="p-6">
+        <h2 className="text-2xl font-bold mb-6 text-center">Users</h2>
+        <div className="overflow-x-auto">
+          <table className="w-full border-collapse border border-gray-300 mt-2">
+            <thead className="bg-gray-200">
+              <tr>
+                <th className="border border-gray-300 px-4 py-2">SN.</th>
+                <th className="border border-gray-300 px-4 py-2">Customer ID</th>
+                <th className="border border-gray-300 px-4 py-2">Email</th>
+                <th className="border border-gray-300 px-4 py-2">Remove</th>
+              </tr>
+            </thead>
+            <tbody>
+                <tr>
+                  <td colSpan="4" className="text-center py-4">
+                    No users available
+                  </td>
+                </tr>
+            </tbody>
+          </table>
+        </div>
     </div>
   )
 }
